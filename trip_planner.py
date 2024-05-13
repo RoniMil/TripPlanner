6f13e4e0e0cc1f1c61d72304317a1c0258adbac4
Roni Milstein <milsteinroni@gmail.com>
2024-05-13 17:56:36 +0300

    refactor flights and hotel retrieval into one function, added comments, inserted hardcoded values for debugging

--- trip_planner.py
+++ trip_planner.py
@@ -1,10 +1,11 @@
 import requests
 from datetime import datetime
+import re
 
 OPENAI_API_KEY = "sk-proj-qBbquYjeeRcbwcs8C1IHT3BlbkFJLZHyfNMDcKE3xW9wWaNr"
-SERPAPI_API_KEY = "1d6a5130ff1de9d0d819c13cb83d8b5b3a7edd233c912f6065052cbeceb6b1b6"
+SERPAPI_API_KEY = "7d278715b0f786d37f8acd830faf6519cfe97706360de96564488c2f9593edbd"
 SEARCH_PROMPT = "suggest me a place to visit in the month: {month} and for the trip type: {trip_type}. return only the location AS A STRING without further information. THE PLACE MUST NOT BE IN THE FOLLOWING LIST: {cur_locations}"
-AIRPORT_PROMPT = 'Give me the international airport codes for {location}. RETURN ONLY THE CODES AND NOTHING ELSE'
+AIRPORT_PROMPT = "Give me the international airport codes for {location}. RETURN ONLY THE CODES AND NOTHING ELSE"
 
 
 # calls on chatGPT to get a trip destination for the given month and trip type. also prevents it from returning a destination already added.
@@ -17,12 +18,15 @@ def get_trip_suggestion(cur_locations, month, trip_type):
             "messages": [
                 {
                     "role": "user",
-                    "content": SEARCH_PROMPT.format(cur_locations=cur_locations, month=month, trip_type=trip_type),
+                    "content": SEARCH_PROMPT.format(
+                        cur_locations=cur_locations, month=month, trip_type=trip_type
+                    ),
                 },
             ],
         },
     )
     return response.json()["choices"][0]["message"]["content"]
+
 
 # returns a list of relevant airports for the destination locations for use in google flights search
 def search_airports(locations):
@@ -41,45 +45,94 @@ def search_airports(locations):
     )
     return response.json()["choices"][0]["message"]["content"]
 
+
 # searches google flights for flights from the given airports (of the found destinations) on the given trip dates whose total round trip time is leq than the given max_price argument.
 # returns the cheapest option
 def search_flights(airports, outbound_date, return_date, max_price):
     response = requests.get(
         "https://serpapi.com/search?engine=google_flights",
-        params={"outbound_date": outbound_date, "return_date": return_date, "max_price": max_price, "departure_id": "TLV", "arrival_id": airports,"api_key": SERPAPI_API_KEY},
+        params={
+            "outbound_date": outbound_date,
+            "return_date": return_date,
+            "max_price": max_price,
+            "departure_id": "TLV",
+            "arrival_id": airports,
+            "api_key": SERPAPI_API_KEY,
+        },
     )
     res = response.json()
     best_flights = res.get("best_flights", [])
     other_flights = res.get("other_flights", [])
     flights = best_flights + other_flights
+    # filter only flights that have a price attribute
+    flights = list(filter(lambda d: "price" in d, flights))
+    # return min price flight
     return min(flights, key=lambda x: x["price"]) if flights else None
 
-# this function calls on the api search while there are less than 5 valid destinations
-def get_valid_destinations(month, trip_type, outbound_date, return_date, budget):
-    seen_destinations = []
-    destinations = {}
+# searches google hotels for a hotel in the given destination on the given trip dates. the price per night is leq than max_price argument.
+# returns the most expensive option
+def search_hotels(destination, check_in_date, check_out_date, max_price):
+    response = requests.get(
+        "https://serpapi.com/search?engine=google_hotels",
+        params={
+            "q": destination,
+            "check_in_date": check_in_date,
+            "check_out_date": check_out_date,
+            "max_price": max_price,
+            "api_key": SERPAPI_API_KEY,
+        },
+    )
+    res = response.json()
+    properties = res.get("properties", [])
+    return (
+        max(properties, key=lambda x: x["total_rate"]["lowest"]) if properties else None
+    )
 
-    # continues search while there are less than 5 destinations fitting the search parameters
-    while len(destinations) < 5:
+# this function retrieves valid trips according to given parameters
+def get_valid_trips(
+    month, trip_type, outbound_date_str, return_date_str, total_days, budget
+):
+    # list of destinations already given as query
+    seen_destinations = []
+    # list of returned trips
+    trips = {}
+
+    # continues search while there are less than 5 trips fitting the search parameters
+    while len(trips) < 5:
         destination = get_trip_suggestion(seen_destinations, month, trip_type)
         seen_destinations.append(destination)
         airports = search_airports(destination)
-        flight_info = search_flights(airports, outbound_date, return_date, budget)
+        flight_info = search_flights(
+            airports, outbound_date_str, return_date_str, budget
+        )
+        # found flights fitting the destination returned by query
         if flight_info:
-            print(f"Cheapest flight to {destination}: {flight_info['price']} USD") 
-            destinations[destination] = flight_info  
+            max_price_per_night = int(
+                (int(budget) - int(flight_info["price"])) / total_days
+            )
+            hotel_info = search_hotels(
+                destination, outbound_date_str, return_date_str, max_price_per_night
+            )
+            # found hotel fitting the destination returned by query
+            if hotel_info:
+                trips[destination] = (
+                    flight_info,
+                    hotel_info,
+                    int(flight_info["price"])
+                    + int(re.sub(r"[^\d]", "", hotel_info["total_rate"]["lowest"])),
+                )
 
-    return destinations          
+    return trips
 
 
 # this function asks the user for the trip parameters and performs the search. retrieves the destinations the are within trip parameters
 def plan_trip(debug=False):
-    if debug: 
+    if debug:
         start_date = "2024-06-10"
         end_date = "2024-06-24"
         budget = "2000"
         trip_type = "beach"
-    else:    
+    else:
         start_date = input("Enter the start date of your trip (YYYY-MM-DD): ")
         end_date = input("Enter the end date of your trip (YYYY-MM-DD): ")
         budget = float(input("Enter your total budget in USD: "))
@@ -87,13 +140,26 @@ def plan_trip(debug=False):
 
     month = datetime.strptime(start_date, "%Y-%m-%d").strftime("%B")
 
-    outbound_date = datetime.strptime(start_date, "%Y-%m-%d").strftime("%Y-%m-%d")
-    return_date = datetime.strptime(end_date, "%Y-%m-%d").strftime("%Y-%m-%d")
+    outbound_date = datetime.strptime(start_date, "%Y-%m-%d")
+    return_date = datetime.strptime(end_date, "%Y-%m-%d")
 
-    destinations = get_valid_destinations(month, trip_type, outbound_date, return_date, budget)
+    outbound_date_str = outbound_date.strftime("%Y-%m-%d")
+    return_date_str = return_date.strftime("%Y-%m-%d")
 
-    print(destinations)          
+    total_days = (return_date - outbound_date).days
 
+    if debug:
+        trips = {
+            "Duckville": ("Flight 101", "Quack Inn", 1500),
+            "Pondtown": ("Flight 202", "Duck Pond Resort", 1800),
+            "Featherton": ("Flight 303", "Feather Nest Hotel", 1999),
+            "Mallard Meadows": ("Flight 404", "Mallard Mansion", 1750),
+            "Beak City": ("Flight 505", "Beak Boulevard Suites", 1600),
+        }
+    else:
+        trips = get_valid_trips(
+            month, trip_type, outbound_date_str, return_date_str, total_days, budget
+        )
 
 if __name__ == "__main__":
-    plan_trip(debug = True)
+    plan_trip(debug=True)
